03e362a084be04f91ade543513dbb719f19c8f00
Blake Gentry <blakesgentry@gmail.com>
2024-04-20 16:07:53 -0500

    exclude devtools in prod NODE_ENV

--- ui/src/components/Root.tsx
+++ ui/src/components/Root.tsx
@@ -1,39 +1,30 @@
 import { Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 import Layout from "@components/Layout";
-import { PropsWithChildren } from "react";
+import React, { PropsWithChildren, Suspense } from "react";
 
-// import { Loader, useLoader } from "@tanstack/react-loaders";
-// import { getSession } from "@services/auth";
-// import { SignOutButton } from "./SignOutButton/SignOutButton";
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+        }))
+      );
 
 type RootProps = PropsWithChildren<object>;
 
 export const Root = (_: RootProps) => {
-  // const [session] = useLoader({ key: sessionLoader.key });
-
   return (
     <>
-      <TanStackRouterDevtools position="bottom-right" />
+      <Suspense>
+        <TanStackRouterDevtools position="bottom-right" />
+      </Suspense>
 
       <Layout>
         <Outlet />
       </Layout>
-      {/* <div className="card">
-        <button onClick={() => setJobState(JobState.Completed)}>
-          Show Completed Jobs
-        </button>
-      </div>
-      <JobList jobState={jobState} /> */}
-      {/* // {session.status === "success" ? <SignOutButton /> : null} */}
     </>
   );
 };
-
-// export const sessionLoader = new Loader({
-//   key: "session",
-//   loader: () => {
-//     return getSession();
-//   },
-// });
