ad6f37947e75dbb7d5a97c145444309ccea1c6dd
Blake Gentry <blakesgentry@gmail.com>
2024-06-09 15:20:24 -0500

    add useFeature hook

---
+++ ui/src/hooks/use-feature.ts
@@ -0,0 +1,28 @@
+import { useMemo } from "react";
+
+const isNil = (val: string | null | undefined): boolean =>
+  val === undefined || val === null;
+
+const getBoolVal = (val?: string | boolean | null): boolean => {
+  if (typeof val === "string") {
+    return val === "true";
+  }
+
+  return !!val;
+};
+
+const useFeature = (name: string, enabled = false): boolean => {
+  const featureEnabled = useMemo((): boolean => {
+    const localStorageValue = window?.localStorage?.getItem(`FEATURE_${name}`);
+    const envVar: string | undefined = import.meta.env[`VITE_FEATURE_${name}`];
+
+    if (!isNil(localStorageValue)) return getBoolVal(localStorageValue);
+    if (!isNil(envVar)) return getBoolVal(envVar);
+
+    return !!enabled;
+  }, [name, enabled]);
+
+  return featureEnabled;
+};
+
+export default useFeature;
