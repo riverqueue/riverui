b0f8baebdc3faecd51f53da20ba44a14541518ff
Blake Gentry <blakesgentry@gmail.com>
2024-06-19 16:17:49 -0500

    text color tweaks on queue list

--- ui/src/components/QueueList.tsx
+++ ui/src/components/QueueList.tsx
@@ -67,21 +67,21 @@ const QueueList = ({
             <tbody className="divide-y divide-slate-200 bg-white dark:divide-slate-800 dark:bg-slate-900">
               {queues.map((queue) => (
                 <tr key={queue.name}>
-                  <td className="w-full max-w-0 py-2 pl-4 pr-3 text-sm font-medium text-slate-900 dark:text-slate-100 sm:w-auto sm:max-w-none sm:pl-0">
-                    <span className="font-mono font-semibold text-slate-700 dark:text-slate-300">
+                  <td className="w-full max-w-0 py-2 pl-4 pr-3 text-sm font-medium text-slate-700 dark:text-slate-300 sm:w-auto sm:max-w-none sm:pl-0">
+                    <span className="font-mono font-semibold dark:text-slate-100">
                       {queue.name}
                     </span>
                     <dl className="font-normal md:hidden">
                       <dt className="sr-only sm:hidden">Available</dt>
-                      <dd className="mt-1 truncate text-slate-700 dark:text-slate-300 sm:hidden">
+                      <dd className="mt-1 truncate sm:hidden">
                         {queue.countAvailable} available
                       </dd>
                       <dt className="sr-only sm:hidden">Running</dt>
-                      <dd className="mt-1 truncate text-slate-700 dark:text-slate-300 sm:hidden">
+                      <dd className="mt-1 truncate sm:hidden">
                         {queue.countRunning} running
                       </dd>
                       <dt className="sr-only">Created</dt>
-                      <dd className="mt-1 truncate text-slate-700 dark:text-slate-300">
+                      <dd className="mt-1 truncate">
                         <RelativeTimeFormatter
                           time={queue.createdAt}
                           addSuffix
@@ -90,20 +90,20 @@ const QueueList = ({
                       </dd>
                     </dl>
                   </td>
-                  <td className="hidden px-3 py-2 text-right text-sm text-slate-500 dark:text-slate-400 md:table-cell">
+                  <td className="hidden px-3 py-2 text-right text-sm text-slate-500 dark:text-slate-300 md:table-cell">
                     <RelativeTimeFormatter
                       time={queue.createdAt}
                       addSuffix
                       includeSeconds
                     />
                   </td>
-                  <td className="hidden px-3 py-2 text-right text-sm text-slate-500 dark:text-slate-400 sm:table-cell">
+                  <td className="hidden px-3 py-2 text-right text-sm text-slate-500 dark:text-slate-300 sm:table-cell">
                     {queue.countAvailable}
                   </td>
-                  <td className="hidden px-3 py-2 text-right text-sm text-slate-500 dark:text-slate-400 sm:table-cell">
+                  <td className="hidden px-3 py-2 text-right text-sm text-slate-500 dark:text-slate-300 sm:table-cell">
                     {queue.countRunning}
                   </td>
-                  <td className="table-cell px-3 py-2 text-sm text-slate-500 dark:text-slate-400">
+                  <td className="table-cell px-3 py-2 text-sm text-slate-500 dark:text-slate-300">
                     {queue.pausedAt ? "Paused" : "Active"}
                   </td>
                   <td className="py-2 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
