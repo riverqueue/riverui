9c22a9b0963c6ec698a71265bb7051a2017bf191
Brandur <brandur@brandur.org>
2024-06-27 21:09:17 -0700

    Use Go validator framework to succinctly validate requests
    A general problem with Go is that validating things is a very noisy
    affair involving long lists of if statements combined with error returns
    for every field on a struct.
    
    A technique that we've been using for a while is to use the Go validator
    framework [1] that allows fields to be tagged with succinct validation
    syntax for a variety of different things. e.g.
    
        type jobCancelRequest struct {
            JobIDs []int64String `json:"ids" validate:"required,min=1,max=1000"`
        }
    
    I'm not sure the use of something like this is necessary for a project
    that's a dependency like core River itself, but but internal use on more
    of an "application" project like River UI, it might be helpful.
    
    We combine the validator with the new API framework from #63 so that
    incoming request structs are validated automatically for every endpoint,
    which shaves a lot of lines of otherwise necessary validation code out
    of individual API endpoint definitions.
    
    [1] https://github.com/go-playground/validator?tab=readme-ov-file

---
+++ internal/validate/validate.go
@@ -0,0 +1,114 @@
+// Package validate internalizes Go Playground's Validator framework, setting
+// some common options that we use everywhere, providing some useful helpers,
+// and exporting a simplified API.
+package validate
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+// WithRequiredStructEnabled can be removed once validator/v11 is released.
+var validate = validator.New(validator.WithRequiredStructEnabled()) //nolint:gochecknoglobals
+
+func init() { //nolint:gochecknoinits
+	validate.RegisterTagNameFunc(preferPublicName)
+}
+
+// PublicFacingMessage builds a complete error message from a validator error
+// that's suitable for public-facing consumption.
+//
+// I only added a few possible validations to start. We'll probably need to add
+// more as we go and expand our usage.
+func PublicFacingMessage(validatorErr error) string {
+	var message string
+
+	//nolint:errorlint
+	if validationErrs, ok := validatorErr.(validator.ValidationErrors); ok {
+		for _, fieldErr := range validationErrs {
+			switch fieldErr.Tag() {
+			case "lte":
+				fallthrough // lte and max are synonyms
+			case "max":
+				kind := fieldErr.Kind()
+				if kind == reflect.Ptr {
+					kind = fieldErr.Type().Elem().Kind()
+				}
+
+				switch kind {
+				case reflect.Float32, reflect.Float64, reflect.Int, reflect.Int32, reflect.Int64:
+					message += fmt.Sprintf(" Field `%s` must be less than or equal to %s.",
+						fieldErr.Field(), fieldErr.Param())
+
+				case reflect.Slice, reflect.Map:
+					message += fmt.Sprintf(" Field `%s` must contain at most %s element(s).",
+						fieldErr.Field(), fieldErr.Param())
+
+				case reflect.String:
+					message += fmt.Sprintf(" Field `%s` must be at most %s character(s) long.",
+						fieldErr.Field(), fieldErr.Param())
+
+				default:
+					message += fieldErr.Error()
+				}
+
+			case "gte":
+				fallthrough // gte and min are synonyms
+			case "min":
+				kind := fieldErr.Kind()
+				if kind == reflect.Ptr {
+					kind = fieldErr.Type().Elem().Kind()
+				}
+
+				switch kind {
+				case reflect.Float32, reflect.Float64, reflect.Int, reflect.Int32, reflect.Int64:
+					message += fmt.Sprintf(" Field `%s` must be greater or equal to %s.",
+						fieldErr.Field(), fieldErr.Param())
+
+				case reflect.Slice, reflect.Map:
+					message += fmt.Sprintf(" Field `%s` must contain at least %s element(s).",
+						fieldErr.Field(), fieldErr.Param())
+
+				case reflect.String:
+					message += fmt.Sprintf(" Field `%s` must be at least %s character(s) long.",
+						fieldErr.Field(), fieldErr.Param())
+
+				default:
+					message += fieldErr.Error()
+				}
+
+			case "required":
+				message += fmt.Sprintf(" Field `%s` is required.", fieldErr.Field())
+
+			default:
+				message += fmt.Sprintf(" Validation on field `%s` failed on the `%s` tag.", fieldErr.Field(), fieldErr.Tag())
+			}
+		}
+	}
+
+	return strings.TrimSpace(message)
+}
+
+// StructCtx validates a structs exposed fields, and automatically validates
+// nested structs, unless otherwise specified and also allows passing of
+// context.Context for contextual validation information.
+func StructCtx(ctx context.Context, s any) error {
+	return validate.StructCtx(ctx, s)
+}
+
+// preferPublicName is a validator tag naming function that uses public names
+// like a field's JSON tag instead of actual field names in structs.
+// This is important because we sent these back as user-facing errors (and the
+// users submitted them as JSON/path parameters).
+func preferPublicName(fld reflect.StructField) string {
+	name, _, _ := strings.Cut(fld.Tag.Get("json"), ",")
+	if name != "" && name != "-" {
+		return name
+	}
+
+	return fld.Name
+}
