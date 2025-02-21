de966c270734adf587420deadbede3a797c93e15
Blake Gentry <bgentry@users.noreply.github.com>
2025-02-21 14:13:57 -0600

    Handle multiline error details, improve display (#291)
    * job factory: realistic sample trace for better storybook pages
    
    * JobDetails: improve styling of attempt error section + traces
    
    * JobAttemptErrors: improve display of multiline errors, stack trace header
    
    * JobAttemptError: handle multiline error messages w/ scrolling
    
    Fixes #290.

--- src/components/JobAttemptErrors.tsx
+++ src/components/JobAttemptErrors.tsx
@@ -1,6 +1,7 @@
-import { Job } from "@services/jobs";
+import { type AttemptError, Job } from "@services/jobs";
 import RelativeTimeFormatter from "./RelativeTimeFormatter";
 import { useState } from "react";
+import clsx from "clsx";
 
 type JobAttemptErrorsProps = {
   job: Job;
@@ -14,13 +15,17 @@ export default function JobAttemptErrors({ job }: JobAttemptErrorsProps) {
     ? job.errors.slice().reverse()
     : job.errors.slice(-1 * defaultErrorDisplayCount).reverse();
 
+  const isMultilineError = (error: AttemptError) => {
+    return error.error.includes("\n");
+  };
+
   return (
-    <div className="sm:col-span-2">
-      <div className="px-4 lg:px-0">
+    <div className="sm:col-span-2 border-slate-100 dark:border-slate-800 border-t py-6 px-4 sm:px-0">
+      <div className="lg:px-0">
         <dt className="text-sm font-medium leading-6 text-slate-900 dark:text-slate-100">
           Errors
         </dt>
-        <dd className="mt-1 text-sm leading-6  text-slate-700 dark:text-slate-300 sm:mt-2">
+        <dd className="mt-1 text-sm leading-6 text-slate-700 dark:text-slate-300 sm:mt-2">
           {job.errors.length === 0 ? (
             <>No errors</>
           ) : (
@@ -30,19 +35,34 @@ export default function JobAttemptErrors({ job }: JobAttemptErrorsProps) {
                 className="divide-y divide-slate-300 dark:divide-slate-700"
               >
                 {errorsToDisplay.map((error) => (
-                  <li key={error.attempt} className="p-4 sm:p-6">
+                  <li key={error.attempt} className="py-4 sm:py-6">
                     <div className="flex items-start">
                       <p className="font-mono font-medium leading-5 text-slate-900 dark:text-slate-100">
                         {error.attempt.toString()}
                       </p>
-                      <div className="ml-4">
-                        <h5 className="mb-4 font-mono text-sm font-medium text-slate-900 dark:text-slate-100">
+                      <div className="ml-4 flex-1 max-w-full min-w-0">
+                        <h5
+                          className={clsx(
+                            "font-mono text-sm font-medium text-slate-900 dark:text-slate-100 mb-2",
+                            isMultilineError(error) &&
+                              "block whitespace-pre bg-slate-300/20 dark:bg-slate-700/20 px-4 py-2 -mt-2 rounded-md max-h-80 h-min overflow-auto resize-y"
+                          )}
+                          aria-description="Error message"
+                        >
                           {error.error}
                         </h5>
                         {error.trace && (
-                          <pre className="max-h-20 overflow-scroll bg-slate-300/10 text-sm text-slate-700 dark:bg-slate-700/10 dark:text-slate-300">
-                            {error.trace}
-                          </pre>
+                          <>
+                            <h6 className="mt-4 mb-2 text-xs font-semibold text-slate-600 dark:text-slate-400">
+                              Stack Trace:
+                            </h6>
+                            <pre
+                              className="w-full h-min max-h-80 bg-slate-300/10 text-sm text-slate-700 dark:bg-slate-700/10 dark:text-slate-300 px-4 py-2 whitespace-pre overflow-x-auto resize-y"
+                              aria-description="Stack trace"
+                            >
+                              {error.trace}
+                            </pre>
+                          </>
                         )}
                         <p className="mt-4 text-sm text-slate-700 dark:text-slate-300">
                           <RelativeTimeFormatter time={error.at} addSuffix />
