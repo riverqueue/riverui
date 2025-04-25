b2611ab42ce99620b292bbe23c3b371524f73fc5
Blake Gentry <blakesgentry@gmail.com>
2025-04-24 21:18:07 -0500

    fix tests

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,3 +1,21 @@
 import "@testing-library/jest-dom";
+import * as matchers from "@testing-library/jest-dom/matchers";
+import { cleanup } from "@testing-library/react";
+import { afterEach, expect } from "vitest";
 
-// This import automatically extends Vitest's expect with DOM testing matchers
+// Extends Vitest's expect method with methods from react-testing-library
+expect.extend(matchers);
+
+// Mock ResizeObserver which is not available in the test environment
+class ResizeObserverMock {
+  disconnect() {}
+  observe() {}
+  unobserve() {}
+}
+
+global.ResizeObserver = ResizeObserverMock;
+
+// Cleanup after each test case
+afterEach(() => {
+  cleanup();
+});
