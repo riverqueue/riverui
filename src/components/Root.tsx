a7475635d88f70aaafb37773d81e293088234226
Blake Gentry <bgentry@users.noreply.github.com>
2025-05-08 19:16:30 -0500

    switch out tanstack router devtools pkg per warning (#350)
    This package was renamed per the deprecation warning. This fixes it.

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -7,7 +7,7 @@ const TanStackRouterDevtools =
     ? () => null // Render nothing in production
     : React.lazy(() =>
         // Lazy load in development
-        import("@tanstack/router-devtools").then((res) => ({
+        import("@tanstack/react-router-devtools").then((res) => ({
           default: res.TanStackRouterDevtools,
         })),
       );
